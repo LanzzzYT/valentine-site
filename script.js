cded9bafd3efacf3e67fa6ee3e4cc63b57ef4432
lanzluther <70057216+LanzzzYT@users.noreply.github.com>
2026-02-13 22:51:29 +0800

    Add background music playback to openLetter function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function openLetter() {
     const letter = document.querySelector(".letter");
+    const music = document.getElementById("bgMusic");
+
     letter.style.display = "block";
+
+    music.play().catch(() => {
+        console.log("Autoplay blocked until user interacts.");
+    });
 }
