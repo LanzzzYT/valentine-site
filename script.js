e5529c2ada88448e4a761ca648b41457d50461fb
lanzluther <70057216+LanzzzYT@users.noreply.github.com>
2026-02-14 00:46:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ function openLetter() {
     // Show paper
     paper.style.display = "block";
 
-    // Show CD top-right
+    // Show CD small top-right
     cdContainer.classList.add("show");
 
     // Play music & rotate CD
     music.play().catch(() => console.log("Music blocked until interaction."));
     cd.classList.add("playing");
+}
+
+// Secret love note function
+function showSecret() {
+    const note = document.getElementById("secretNote");
+    note.classList.add("show");
+
+    // Fade out after 5 seconds
+    setTimeout(() => {
+        note.classList.remove("show");
+    }, 5000);
 }
